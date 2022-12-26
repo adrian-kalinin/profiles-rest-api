2605ab6cce357df9eca4bc7c00743613ef72d413
Adrian Kalinin <adriankalinin@protonmail.com>
2022-12-26 23:00:50 +0200

    Add examples to a separate app

---
+++ examples/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from examples import views
+
+router = DefaultRouter()
+router.register("hello-viewset", views.HelloViewSet, basename="hello-viewset")
+
+urlpatterns = [
+    path("hello-apiview/", views.HelloApiView.as_view()),
+    path("", include(router.urls)),
+]
