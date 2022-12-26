96317dcf06abe21bde6c8133e055e8d2e1706c30
Adrian Kalinin <adriankalinin@protonmail.com>
2022-12-26 22:50:21 +0200

    Add login view

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from rest_framework import filters, status, viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
 from rest_framework.views import APIView
 
 from profiles import models, permissions, serializers
@@ -98,3 +100,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsUserProfileOwnerOrReadOnly,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ("email", "name")
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Create user authentication tokens"""
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
